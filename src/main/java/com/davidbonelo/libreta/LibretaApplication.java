d263d1c1605c27caea693d65e14eb7b385120e80
David Bonelo <bonelodev@gmail.com>
2023-03-06 20:16:09 -0500

    Initial commit

---
+++ src/main/java/com/davidbonelo/libreta/LibretaApplication.java
@@ -0,0 +1,13 @@
+package com.davidbonelo.libreta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibretaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibretaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidbonelo/libreta/LibretaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidbonelo.libreta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibretaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
