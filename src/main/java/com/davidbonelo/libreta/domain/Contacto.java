56453129ec9a5e91252dda644d250fdadcf75c4d
David Bonelo <bonelodev@gmail.com>
2023-03-07 18:14:47 -0500

    Add domain entities

---
+++ src/main/java/com/davidbonelo/libreta/domain/Contacto.java
@@ -0,0 +1,67 @@
+package com.davidbonelo.libreta.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.CascadeType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contacto")
+public class Contacto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer cnt_id;
+
+
+    /**
+     * Nombre del contacto
+     */
+    @Column(name = "cnt_nombre", nullable = false, length = 100)
+    private String nombre;
+
+    /**
+     * Apellidos del contacto
+     */
+    @Column(name = "cnt_apellido", nullable = false, length = 100)
+    private String apellido;
+
+    /**
+     * Fecha y hora en que la tupla ha sido creada
+     */
+    @Column(name = "cnt_created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    /**
+     * Fecha y hora en que la tupla ha sido actualizada por última vez
+     */
+    @Column(name = "cnt_updated_at")
+    private Instant updatedAt;
+
+    /**
+     * Punto de enlace entre la entidad del Contacto y Teléfono (un contacto puede tener muchos números de teléfono)
+     */
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            targetEntity = Telefono.class,
+            cascade = CascadeType.REMOVE,
+            mappedBy = "contacto"
+    )
+    @JsonManagedReference
+    private List<Telefono> telefonos = new ArrayList<>();
+
+}
---
+++ src/main/java/com/davidbonelo/libreta/domain/Telefono.java
@@ -0,0 +1,53 @@
+package com.davidbonelo.libreta.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "telefono")
+public class Telefono {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    /**
+     * Punto de enlace con la entidad Contacto (un contacto puede tener muchos número de teléfono)
+     */
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Contacto.class, optional = false)
+    @JoinColumn(name = "tel_contacto_id", nullable = false)
+    @JsonBackReference
+    private Contacto contacto;
+
+    /**
+     * Número de teléfono
+     */
+    @Column(name = "tel_telefono", nullable = false, length = 30)
+    private String telefono;
+
+    /**
+     * Fecha y hora en que la tupla ha sido creada
+     */
+    @Column(name = "tel_created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    /**
+     * Fecha y hora en que la tupla ha sido actualizada por última vez
+     */
+    @Column(name = "tel_updated_at")
+    private Instant updatedAt;
+}
